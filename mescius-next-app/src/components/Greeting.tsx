e3f01146408c3588ff0b53af87ee5cbdb71faa1d
NicklasPhoenix <130753410+NicklasPhoenix@users.noreply.github.com>
2025-07-02 16:29:44 +0200

    Update Greeting.tsx

--- mescius-next-app/src/components/Greeting.tsx
+++ mescius-next-app/src/components/Greeting.tsx
@@ -1,4 +1,4 @@
 // src/components/Greeting.tsx
 export default function Greeting() {
-  return <p>This greeting comes from a reusable component!</p>;
+  return <p>Hello from a reusable component!</p>;
 }
